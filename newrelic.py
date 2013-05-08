16a1ad0f43e28f58af4c67bb3094e2d969c47f0d
bryan kennedy <bkennedy@smm.org>
2013-05-08 10:05:28 -0500

    Adding code to read the New Relic build details
    The recipes that use this script will send along a dict with the details
    inside

--- newrelic.py
+++ newrelic.py
@@ -8,15 +8,17 @@ import urllib
 import urllib2
 
 
-from fabric_colors.environment import set_target_env
-
 @task
-def report_deploy():
+def report_deploy(app_values):
     """
     Get the New Relic deploy XML. This records a deploy action with
     the New Relic monitoring tool.
+
+    Args:
+        A dictionary of the New Relic Parameters.
     """
     deploy_url = "https://rpm.newrelic.com/deployments.xml"
+    app_values = urllib.urlencode(app_values)
     request = urllib2.Request(deploy_url, app_values)
     request.add_header('x-api-key', env.newrelic['API_KEY'])
-    urllib2.urlopen(req)
+    urllib2.urlopen(request)
