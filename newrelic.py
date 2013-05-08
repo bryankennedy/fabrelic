ff8938767f0377c1933ebec64994ae53f764ea3c
bryan kennedy <bkennedy@smm.org>
2013-05-08 12:11:01 -0500

    Troubleshooting deploy code and dict passing

--- newrelic.py
+++ newrelic.py
@@ -17,8 +17,17 @@ def report_deploy(api_key, app_values):
     Args:
         A dictionary of the New Relic Parameters.
     """
-    deploy_url = "https://rpm.newrelic.com/deployments.xml"
+    deploy_url = 'https://rpm.newrelic.com/deployments.xml'
     app_values = urllib.urlencode(app_values)
-    request = urllib2.Request(deploy_url, app_values)
-    request.add_header('x-api-key', api_key)
-    urllib2.urlopen(request)
+    #request = urllib2.Request(deploy_url)
+    #request = urllib2.Request(deploy_url, app_values)
+    #print api_key
+    #request.add_header('x-api-key', api_key)
+    #urllib2.urlopen(request)
+    request = urllib2.Request(
+        deploy_url,
+        urllib.urlencode(app_values)
+        #urllib.urlencode({'deployment[application_id]': 'app_name'})
+    )
+    request.add_header('X-api-key', api_key)
+    response = urllib2.urlopen(request)
